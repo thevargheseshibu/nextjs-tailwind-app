0b5fc7f38f70ce0dbdbd0056d0be30e9b53383c9
Varghese <thevargheseshibu@gmail.com>
2025-05-19 11:57:33 +0530

    Feat: NPM modules

---
+++ next.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    // next.config.js
+    images: {
+        domains: ['media.istockphoto.com','www.etpgroup.com'],
+      },
+};
+
+export default nextConfig;
