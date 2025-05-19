09b5b119f3dc58b4e6b6901e5677bb1ef9ce8559
Varghese <thevargheseshibu@gmail.com>
2025-05-19 11:57:14 +0530

    Feat: Initial Config

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+const plugin = require('tailwindcss/plugin');
+
+module.exports = {
+  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: 'var(--color-primary)',
+        secondary: 'var(--color-secondary)',
+        accent: 'var(--color-accent)',
+      },
+    },
+  },
+  plugins: [],
+};
