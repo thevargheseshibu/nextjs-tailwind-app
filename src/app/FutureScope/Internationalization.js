dff88cfba0d7146965ccf2d87b8c313376471eca
Varghese <thevargheseshibu@gmail.com>
2025-05-19 11:59:15 +0530

    Feat: env and internationalization

---
+++ src/app/FutureScope/Internationalization.js
@@ -0,0 +1,7 @@
+export const Internationalization = {
+  "contentBlocks": "Content Blocks",
+  "subContent": "Sub Content",
+  "sideContent": "Side Content",
+  "autoFitCards": "Auto Fit Cards"
+}
+  
---
+++ src/app/config/urls.js
@@ -0,0 +1,8 @@
+const EXTERNAL_URLS = {
+    logo: 'https://www.etpgroup.com/wp-content/uploads/2024/04/ETP-Logo-Coloured-1200x628-1.png',
+    social: 'https://img.icons8.com/ios-filled/20/ffffff/',
+    media: 'https://media.istockphoto.com/id/1438634414/photo/business-women-laptop-and-and-happy-team-in-office-for-web-design-collaboration-and-training.jpg?s=2048x2048&w=is&k=20&c=Pd7jf2xPKirZQuJmRWE_Va4bEtmy2crWz0p3L1wrDAM=',
+  };
+  
+  export default EXTERNAL_URLS;
+  
